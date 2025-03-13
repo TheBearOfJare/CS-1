f987828c08dde09893cf93ebaf84c895d2811f94
Jared Gleisner <darkphoton31@gmail.com>
2025-03-13 17:29:46 -0500

    updated chudnovski

--- PI_DAY.java
+++ PI_DAY.java
@@ -1,14 +1,33 @@
-import java.util.*;;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.math.BigDecimal;
+import java.math.MathContext;
 
 public class PI_DAY {
 
-    public static List<Double> chudify (int a, int b) {
-        
-        double Pab = -(6*a - 5)*(2*a - 1)*(6*a - 1);
-        double Qab = 10939058860032000d * Math.pow(a, 3);
-        double Rab = Pab * (545140134*a + 13591409);
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        List<Double> output = new ArrayList<>();
+    public static void save(BigDecimal output, String name) throws FileNotFoundException, UnsupportedEncodingException {
+        PrintWriter writer = new PrintWriter(name, "UTF-8");
+
+        writer.write(output.toPlainString());
+
+    }
+
+    public static List<BigDecimal> chudify (BigDecimal a, BigDecimal b, MathContext depth) {
+        
+        BigDecimal Pab = (a.multiply(BigDecimal.valueOf(6), depth).subtract(BigDecimal.valueOf(5), depth)).multiply(a.multiply(BigDecimal.valueOf(2), depth).subtract(BigDecimal.valueOf(1), depth), depth).multiply(a.multiply(BigDecimal.valueOf(6), depth).subtract(BigDecimal.valueOf(1), depth), depth);
+        
+        BigDecimal Qab = a.pow(3, depth).multiply(BigDecimal.valueOf(10939058860032000l), depth);
+        
+        BigDecimal Rab = Pab.multiply(a.multiply(BigDecimal.valueOf(545140134), depth).add(BigDecimal.valueOf(13591409), depth), depth);
+
+        List<BigDecimal> output = new ArrayList<>();
         output.add(Pab);
         output.add(Qab);
         output.add(Rab);
@@ -16,37 +35,29 @@ public class PI_DAY {
 
     }
     
-    public static List<Double> chudnovskify (int a, int b) {
+    public static List<BigDecimal> chudnovskify (BigDecimal a, BigDecimal b, MathContext depth) {
 
-        double Pab;
-        double Qab;
-        double Rab;
+        BigDecimal Pab;
+        BigDecimal Qab;
+        BigDecimal Rab;
 
-        if (b == a + 1) {
-            Pab = -(6*a - 5)*(2*a - 1)*(6*a - 1);
-            Qab = 10939058860032000d * Math.pow(a, 3);
-            Rab = Pab * (545140134*a + 13591409);
+        BigDecimal m = a.add(b, depth);
+        List<BigDecimal> a_set = chudify(a, m, depth);
+        List<BigDecimal> b_set = chudify(m, b, depth);
+        
+        BigDecimal Pam = a_set.get(0);
+        BigDecimal Qam = a_set.get(1);
+        BigDecimal Ram = a_set.get(2);
 
-        }
-        else {
-            int m = a + b;
-            List<Double> a_set = chudify(a, m);
-            List<Double> b_set = chudify(m, b);
-            
-            double Pam = a_set.get(0);
-            double Qam = a_set.get(1);
-            double Ram = a_set.get(2);
+        BigDecimal Pmb = b_set.get(0);
+        BigDecimal Qmb = b_set.get(1);
+        BigDecimal Rmb = b_set.get(2);
 
-            double Pmb = b_set.get(0);
-            double Qmb = b_set.get(1);
-            double Rmb = b_set.get(2);
+        Pab = Pam.multiply(Pmb, depth);
+        Qab = Qam.multiply(Qmb, depth);
+        Rab = Qmb.multiply(Ram, depth).add(Pam.multiply(Rmb, depth), depth);
 
-            Pab = Pam * Pmb;
-            Qab = Qam * Qmb;
-            Rab = Qmb * Ram + Pam * Rmb;
-        }
-
-        List<Double> output = new ArrayList<>();
+        List<BigDecimal> output = new ArrayList<>();
 
         output.add(Pab);
         output.add(Qab);
@@ -55,23 +66,45 @@ public class PI_DAY {
 
     }
 
-    public static double chudnovsky(int n) {
-        // Chudnovsky algorithm.
-        List<Double> chuds = chudnovskify(1, n);
-        double P1n = chuds.get(0);
-        double Q1n = chuds.get(1);
-        double R1n = chuds.get(2);
-        return (426880 * Math.sqrt(10005) * Q1n) / (13591409*Q1n + R1n);
+    public static BigDecimal chudnovsky(BigDecimal n, MathContext depth) {
+
+        // Chudnovsky algorithm
+        List<BigDecimal> chuds = chudnovskify(BigDecimal.valueOf(1), n, depth);
+        // BigDecimal P1n = chuds.get(0);
+        BigDecimal Q1n = chuds.get(1);
+        BigDecimal R1n = chuds.get(2);
+        return (BigDecimal.valueOf(426880).multiply(BigDecimal.valueOf(10005).sqrt(depth), depth).multiply(Q1n, depth).divide(Q1n.multiply(BigDecimal.valueOf(13591409), depth).add(R1n, depth), depth));
 
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        int depth = 100;
-        System.out.println(depth + " " + chudnovsky(depth));  // 3.141592653589793238462643384
 
-        // for (int n = 1; n < 10; n++) {
-        //     System.out.println("{n} = " + chudnovsky(n));  // 3.14159265358979323846264338...
-        // }
+        // get the number of digits
+        System.out.println("How many million digits?");
+        String input = reader.readLine();
+
+        // time the calculation
+        long start = System.currentTimeMillis();
+
+        int mdigits = Integer.parseInt(input) * 1000000;
+
+        // set the number of digits
+        MathContext depth = new MathContext(mdigits);
+        BigDecimal n = new BigDecimal(mdigits, depth);
+
+        // calculate PI
+        BigDecimal PI = chudnovsky(n, depth);
+
+        System.out.println("Time to calculate: " + (System.currentTimeMillis() - start) + "ms");
+        start = System.currentTimeMillis();
+
+        // save the result
+        save(PI, "PI"+input+"M.txt");
+
+        System.out.println("Time to save: " + (System.currentTimeMillis() - start) + "ms");
+
+        System.out.println("Finished.");
+        //System.out.println(PI);
 
     }
     
--- test.java
+++ test.java
@@ -1,12 +1,24 @@
 import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.math.MathContext;
 
 public class test {
 
     public static void main(String[] args) {
-        long a = 1;
-        int b = 2;
+       
 
-        System.out.println(a+b);
+        MathContext depth = new MathContext(100000000);
+
+        BigDecimal a = new BigDecimal(3, depth);
+        BigDecimal b = BigDecimal.valueOf(10);
+
+        BigDecimal c = b.divide(a, depth);
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
 
 
 
